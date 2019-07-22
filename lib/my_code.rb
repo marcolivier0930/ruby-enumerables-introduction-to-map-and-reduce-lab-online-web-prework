8cfb8868f363a82362372e509d3b4780323ba424
Marc Blanchard <marcblanchard0930@gmail.com>
2019-07-22 17:13:50 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,46 @@
 # My Code here....
+#map 
+def map_to_negativize(source_array)
+  map_like = []
+  i = 0 
+  while i < source_array.length do
+    map_like.push(source_array[i] * -1)
+    i += 1
+  end
+  
+  map_like
+end
+
+def map_to_no_change(source_array)
+  map_like = ["paul", "gurney", "vladimir", "jessica", "chani"]
+  
+  return map_like
+end
+
+
+def map_to_double(source_array)
+  map_like = []
+  i = 0 
+  while i < source_array.length do
+    map_like.push(source_array[i] * 2)
+    i += 1
+  end
+  
+  return map_like
+end
+
+def map_to_square(source_array)
+  map_like = []
+  i = 0 
+  while i < source_array.length do
+    map_like.push(source_array[i] ** 2)
+    i += 1
+  end
+  
+  return map_like
+end
+
+# reduce
+def reduce_to_total(source_array)
+  
+end
