ed0316cf8ef65e2e09b81b2b09e69a0e8ab093bc
Marc Blanchard <marcblanchard0930@gmail.com>
2019-07-22 19:32:27 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,6 +41,24 @@ def map_to_square(source_array)
 end
 
 # reduce
-def reduce_to_total(source_array)
-  
+
+def reduce_to_total(source_array, starting_point = 0)
+  i = 0 
+  while i < source_array.length do
+    starting_point += source_array[i]
+    i += 1 
+  end
+  return starting_point
+end
+
+
+def reduce_to_all_true(source_array)
+  i = 0 
+  while i < source_array.length do
+    if source_array[i] != nil
+      return true
+    else if source_array[i] == nil 
+      return false
+    end
+  end
 end
